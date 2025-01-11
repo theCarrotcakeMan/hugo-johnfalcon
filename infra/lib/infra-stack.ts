eb09aedf2823f4d5807e5857b97a8bcac0df46c1
John Falcón <johngrowscode@gmail.com>
2025-01-10 18:35:55 -0600

    update(deploy-workflow): latest optimizations to deployment workflow

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,67 +1,65 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
-import { HostedZone, ARecord, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { HostedZone, ARecord, RecordTarget, CnameRecord } from 'aws-cdk-lib/aws-route53';
 import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
-import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
+import { HttpOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { join } from 'path';
 import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { CfnOutput } from 'aws-cdk-lib';
 
 export class HugoWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create an S3 bucket for website hosting (private access)
-    const websiteBucket = new Bucket(this, 'HugoWebsiteBucket', {
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: '404.html',
-      blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // Ensure the bucket is private
-    });
-
     // Route custom domain using Route 53
     const domainName = 'growingcode.studio';
     const zone = HostedZone.fromLookup(this, 'HostedZone', { domainName });
 
-    // Create an OAI for CloudFront to securely access the bucket
-    const originAccessIdentity = new OriginAccessIdentity(this, 'OAI');
-    console.log('CloudFront OAI ID:', originAccessIdentity.originAccessIdentityId);
-
-    // Grant read access to the OAI (simplified)
-    websiteBucket.grantRead(originAccessIdentity);
+    // Create an S3 bucket for website hosting (private access)
+    const websiteBucket = new Bucket(this, 'HugoWebsiteBucket', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // Ensure the bucket is private
+    });
 
     // Add an explicit bucket policy for CloudFront (OAI)
     websiteBucket.addToResourcePolicy(new PolicyStatement({
       effect: Effect.ALLOW,
       actions: ['s3:GetObject'],
       resources: [websiteBucket.arnForObjects('*')],
-      principals: [originAccessIdentity.grantPrincipal],
+      principals: [new cdk.aws_iam.ServicePrincipal('cloudfront.amazonaws.com')],
     }));
 
-    // Create an SSL certificate in the 'us-east-1' region for CloudFront
-    const certificate = new cdk.aws_certificatemanager.DnsValidatedCertificate(this, 'SiteCertificate', {
-      domainName: 'growingcode.studio',
-      hostedZone: zone,
-      region: 'us-east-1',
+    // Create an SSL certificate for the custom domain in 'us-east-1'
+    const certificate = new acm.Certificate(this, 'SiteCertificate', {
+      domainName: domainName,
+      validation: acm.CertificateValidation.fromDns(zone),  // Validate ownership via DNS
     });
 
     // CloudFront distribution pointing to the S3 bucket using the OAI
     const distribution = new Distribution(this, 'WebsiteDistribution', {
       defaultBehavior: {
-        origin: new S3Origin(websiteBucket, { originAccessIdentity }),
+        origin: new HttpOrigin(websiteBucket.bucketRegionalDomainName),
         viewerProtocolPolicy: cdk.aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-      domainNames: [domainName],
+      domainNames: [domainName, `www.${domainName}`],
       certificate,
     });
 
-    // Create an A record in Route 53 pointing to the CloudFront distribution
+    // Create an A record pointing the root domain to CloudFront distribution
     new ARecord(this, 'SiteAliasRecord', {
+      zone,
       recordName: domainName,
       target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    });
+
+    // Create an A record for 'www' subdomain pointing to the CloudFront distribution
+    new ARecord(this, 'WWWAliasRecord', {
       zone,
+      recordName: 'www',
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
     });
 
     // Deploy Hugo site to the S3 bucket
@@ -71,5 +69,18 @@ export class HugoWebsiteStack extends cdk.Stack {
       distribution, // Invalidate CloudFront cache after deployment
       distributionPaths: ['/*'],
     });
+
+    // Export the OAI ID as a CloudFormation output
+    new CfnOutput(this, 'OAIID', {
+      value: originAccessIdentity.originAccessIdentityId,
+      description: 'The ID of the CloudFront Origin Access Identity (OAI)',
+    });
+
+    new CfnOutput(this, 'CloudFrontURL', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'URL of the deployed website',
+    });
+    
+
   }
 }
